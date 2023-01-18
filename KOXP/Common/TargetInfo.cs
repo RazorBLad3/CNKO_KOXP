2e44ed70ce8a3293d323349ca93fb0353b49de10
Administrator <Administrator@WIN-6JU078SA8I5>
2023-01-18 22:48:09 +0300

    Add project files.

---
+++ KOXP/Common/TargetInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace KOXP.Common
+{
+    public class TargetInfo
+    {
+        public int Base { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Hp { get; set; }
+        public int MaxHp { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Nation { get; set; }
+        public int State { get; set; }
+        public int Statu { get; set; }
+        public int MoveType { get; set; }
+    }
+}
